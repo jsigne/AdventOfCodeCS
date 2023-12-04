d02b21a6cbc89925c57fdd082afcb28f93459c02
jsigne <josie.signe@alten.com>
2023-12-04 15:12:03 +0100

    [Day2] Init methods to check if a game record is valid

---
+++ AdventOfCode/2023/DayTwo/CubeGame.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode._2023.DayTwo
+{
+    public class CubeGame
+    {
+        private int redCubeNumber { get; set; }
+        private int greenCubeNumber { get; set; }
+        private int blueCubeNumber { get; set; }
+        public CubeGame(int redCubeNumber, int greenCubeNumber, int blueCubeNumber)
+        {
+            this.redCubeNumber = redCubeNumber;
+            this.greenCubeNumber = greenCubeNumber;
+            this.blueCubeNumber = blueCubeNumber;
+        }
+
+        public int processRecords(List<string> records)
+        {
+            return 0;
+        }
+
+
+        private bool IsSetImpossible(Set set)
+        {
+            return set.red > redCubeNumber && set.green > greenCubeNumber && set.blue > blueCubeNumber;
+        }
+
+        private bool IsGamePossible(GameRecord gameRecord)
+        {
+            foreach(Set set in gameRecord.sets)
+            {
+                if (IsSetImpossible(set))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private class Set()
+        {
+            public int red {get; private set;}
+            public int blue {get; private set;}
+            public int green {get; private set;}
+        }
+
+        private class GameRecord()
+        {
+            public int id;
+            public List<Set> sets {get; set;}
+        }
+
+        public static void Main(string[] args)
+        {
+            CubeGame cubeGame = new(12, 13, 14);
+
+            Console.WriteLine("Sum of ids of valid game records");
+
+            string[] cubeGameRecordsDocument = File.ReadAllLines("ressources\\CubeGameRecords.txt");
+
+            Console.WriteLine(cubeGame.processRecords(cubeGameRecordsDocument.ToList()));
+        }
+    }
+}
---
+++ AdventOfCodeTests/CubeGameTests.cs
@@ -0,0 +1,23 @@
+using AdventOfCode._2023.DayTwo;
+
+namespace AdventOfCodeTests
+{
+    public class CubeGameTests
+    {
+
+        [Fact]
+        public void Game_Should_Be_Possible()
+        {
+            //arrange
+            CubeGame cubeGame = new(12, 13, 14);
+            string gameRecord = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green";
+
+            //act
+
+            //assert
+            
+        }
+
+
+    }
+}, 
